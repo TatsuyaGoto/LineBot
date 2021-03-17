b1880e3b22720458142963d7d387965448be5c36
TatsuyaGoto <gtatsu510@gmail.com>
2021-03-17 22:38:05 +0900

    add linepush test

--- src/main/java/com/example/demo/IndividualDaoJdbc.java
+++ src/main/java/com/example/demo/IndividualDaoJdbc.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -29,6 +33,24 @@ public class IndividualDaoJdbc {
 		int rowNumber = jdbc.update("delete from line_member where userid=?", userid);
 
 		return rowNumber;
+
+	}
+
+	public List<String> getLineMember() throws DataAccessException {
+
+		List<Map<String, Object>> getList = jdbc.queryForList("select userid from line_member");
+
+		List<String> memberList = new ArrayList<>();
+
+		for(Map<String, Object> map: getList) {
+
+			String userId = (String)map.get("userid");
+
+			memberList.add(userId);
+		}
+
+		return memberList;
+
 
 	}
 
--- src/main/java/com/example/demo/PushService.java
+++ src/main/java/com/example/demo/PushService.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.Multicast;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 
@@ -18,15 +21,22 @@ public class PushService {
 
 	@Autowired
 	LineMessagingClient lineMessagingClient;
+	@Autowired
+	IndividualDaoJdbc individualDao;
 
 	public void pushMessage(String message) {
 
 		try {
 
 	    	TextMessage textMessage = new TextMessage(message);
-	    	PushMessage pushMessage = new PushMessage("U3ffa70b26534f3f36f59c17b82e7295d", textMessage);
 
-	    	BotApiResponse response = lineMessagingClient.pushMessage(pushMessage).get();
+	    	List<String> getList = individualDao.getLineMember();
+	    	Set<String> lineMember = new HashSet<String>(getList);
+	    	Multicast multicast = new Multicast(lineMember, textMessage);
+	    	BotApiResponse response = lineMessagingClient.multicast(multicast).get();
+
+	    	//PushMessage pushMessage = new PushMessage("U3ffa70b26534f3f36f59c17b82e7295d", textMessage);
+	    	//BotApiResponse response = lineMessagingClient.pushMessage(pushMessage).get();
 
 	      System.out.println(response);
 
